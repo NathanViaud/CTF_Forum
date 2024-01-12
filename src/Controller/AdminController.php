0add4c0566f985deeb28b5752c93e889cb1c21db
VIAUD Nathan <nathan.viaud@inria.fr>
2024-01-12 20:34:09 +0100

    Adding flag page

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'app.admin')]
+class AdminController extends AbstractController
+{
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    #[Route('/flag', name: '.flag', methods: ['GET'])]
+    public function index(): Response
+    {
+
+        $flag = $this->params->get('admin_flag', false);
+
+        return $this->render('admin/index.html.twig', [
+            'flag' => $flag
+        ]);
+    }
+}
